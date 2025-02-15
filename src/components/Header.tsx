a7a662e35212e41ffc1d663b11db2f26c21761ad
spoonmilk <haku31229@gmail.com>
2025-02-14 21:37:14 -0500

    fixing deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="mb-8">
-      <h1 className="text-4xl font-bold mb-2">Happy Valentine's Day!</h1>
+      <h1 className="text-4xl font-bold mb-2">Happy Valentine&apos;s Day!</h1>
       <p className="text-lg">
-        I figured a card wouldn't arrive for a bit, so I thought it would be nice to make you something cute.
+        I figured a card wouldn&apos;t arrive for a bit, so I thought it would be nice to make you something cute.
       </p>
       <p className="text-lg">
-        Turns out I'm really bad at web development, but I hope you like it!
+        Turns out I&apos;m really bad at web development, but I hope you like it!
       </p>
     </header>
   );
